ed0fb3b09acd061b1c85efa0073713e342fc9cfa
T-Gamer <marquesde2013@gmail.com>
2020-07-28 21:37:45 -0300

    Final Aula 2

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,6 +6,12 @@ function CadastroCategoria(){
     return (
       <PageDefault>
          <h1>Cadastro de Categoria</h1>
+         <form>
+           <label>
+             Nome da Categoria:
+           </label>
+           <button>Cadastrar</button>
+         </form>
          <Link to ="/">
            Ir para home
          </Link>
