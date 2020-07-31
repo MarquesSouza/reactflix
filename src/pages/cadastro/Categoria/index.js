5c713382bfab288e46992b434d979486c2e19d4a
T-Gamer <marquesde2013@gmail.com>
2020-07-30 22:10:33 -0300

    aula 4

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import {Link} from 'react-router-dom';
@@ -24,6 +24,34 @@ function CadastroCategoria(){
     inputHanle.target.getAttribute('name'),
     inputHanle.target.value
     )}
+    useEffect(()=>{
+      const URL = 'http://localhost:8080/categorias';
+
+      fetch(URL).then(async (req)=>{
+        const respota = await req.json();
+        setCategorias([
+          ...respota,
+        ]);
+      })
+    /*  setTimeout(()=>{
+        setCategorias([
+          ...categorias,
+          {
+            "id":1,
+            "nome": "Front End",
+            "descricao":"Uma Categoria",
+            "cor":"#cbd1ff"
+            },
+            {
+                "id":2,
+                "nome": "Back End",
+                "descricao":"Uma Categoria",
+                "cor":"#cbd1ff"
+                }
+          ])
+      },4*1000) */
+    },[])
+
   return (
       <PageDefault>
          <h1>Cadastro de Categoria {values.nome}</h1>
@@ -59,7 +87,12 @@ function CadastroCategoria(){
            />
            <Button>Cadastrar</Button>
          </form>
-        
+        {categorias.length===0 &&(
+          <div>
+            Loading...
+          </div>
+
+        )}
       <ul>
         {categorias.map((categoria, indice) => {
           return (
